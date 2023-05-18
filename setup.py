6d0b796b95932f777a4050a672906afd6cb9adcc
Meysam Ahangaran <81852309+me-ahangaran@users.noreply.github.com>
2023-05-18 15:14:03 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from DREAMER import 0.0.1
+
+setup(
+    name='DREAMER',
+    version=__version__,
+
+    url='https://github.com/me-ahangaran/DREAMER',
+    author='Meysam Ahangaran',
+    author_email='ahangar@bu.edu',
+
+    py_modules=['DREAMER'],
+)
